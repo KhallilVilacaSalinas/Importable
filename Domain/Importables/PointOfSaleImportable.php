dcf61d3baa1f6d665d94853c3fe869998a44f067
KhallilVilacaSalinas <khallil.vilaca@gmail.com>
2022-09-10 19:50:22 -0300

    Removendo diretorios

--- Domain/Importables/PointOfSaleImportable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Siv3\Importable\Domain\Importables;
-
-class PointOfSaleImportable
-{
-
-}
--- Model/ImportableInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Siv3\Importable\Model;
-
-interface ImportableInterface
-{
-    public function importableService($menssage);
-}
